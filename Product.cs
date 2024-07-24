3cf4a9a8de775ec43d840372b6055d7062cb3905
KusumShoba <149655613+KusumShoba@users.noreply.github.com>
2024-07-24 20:40:37 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace SampleMvcApp.Models
+{
+
+    public class Product
+    {
+        public int PId { get; set; }
+        public string PName { get; set; } = string.Empty;
+
+        public int PPrice { get; set; }
+        public string PImg { get; set; } = string.Empty;
+    }
+
+
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleMvcApp.Models;
+
+namespace SampleMvcApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductDbContext _productDbContext;
+
+        public ProductController(ProductDbContext productDbContext)
+        {
+            _productDbContext = productDbContext;
+        }
+
+        public IActionResult AllProducts()
+        {
+            var products = _productDbContext.Products.ToList();
+            return View(products);
+        }
+    }
+}
